eb901d2517c8e055f4d7a9777f9869b66a6415be
varfandrey <varf.andrey@hotmail.com>
2017-01-19 21:40:32 +1000

    small fixed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//$(document).scroll(function() {
+//  navbarScroll();
+//});
+//
+//function navbarScroll() {
+//  var y = intro.offsetWidth;
+//  if (y < window.pageYOffset) {
+//      console.log (1);
+//  } else if (y < 10) {
+//
+//  }
+//}
