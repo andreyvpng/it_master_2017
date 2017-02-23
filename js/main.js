ce6cd64f114d0cb2239a6b0d19303d5bc2ac8e7a
varfandrey <varf.andrey@hotmail.com>
2017-02-23 17:54:38 +1000

    123

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-//$(document).scroll(function() {
-//  navbarScroll();
-//});
-//
-//function navbarScroll() {
-//  var y = intro.offsetWidth;
-//  if (y < window.pageYOffset) {
-//      console.log (1);
-//  } else if (y < 10) {
-//
-//  }
-//}
+/*
+ * ARTICLE_HEADER_SCROLL
+ */
+var 
+	h_scroll,
+	last_scroll,
+	headerHeight,
+	object = $("#header");
+
+$(document).scroll(function() {
+	h_scroll = window.pageYOffset;
+	headerHeight = header.offsetHeight;
+
+	if(headerHeight < h_scroll){
+		if(headerHeight < h_scroll + 100){
+			if( last_scroll > h_scroll )
+				object.css({"opacity": "1"});
+			else
+				object.css({"opacity": "0"});
+		}
+	}
+
+	last_scroll = h_scroll;
+});
